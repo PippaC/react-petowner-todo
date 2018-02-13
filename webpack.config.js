1c91b0843e62d20bb38b654798cbae9eba9aaebf
Lindsey <lindsey@Lindseys-MacBook-Pro.local>
2018-02-13 18:19:20 +0800

    Add extract-text-webpack-plugin to build css bundler.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   context: __dirname,
-  entry: ['./js/ClientApp.jsx'],
+  entry: ['./js/ClientApp.jsx', './public/styles/pie.scss'],
   devtool: process.env.NODE_ENV === 'development' ? 'cheap-eval-source-map' : false,
   output: {
     path: path.resolve(__dirname, 'public'),
@@ -23,7 +24,7 @@ const config = {
     reasons: true,
     chunks: false
   },
-  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin()],
+
   module: {
     rules: [
       /*{
@@ -35,10 +36,39 @@ const config = {
       {
         test: /\.jsx?$/,
         loader: 'babel-loader'
+      },
+      { // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1',
+        }),
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), 
+    new webpack.NamedModulesPlugin(),
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'dist/main.css',
+      allChunks: true
+    })
+  ]
 };
+
+require('babel-core/register')({
+  presets: ['es2015', 'react']
+});
+require.extensions['.scss'] = () => {
+  return;
+};
+require.extensions['.css'] = () => {
+  return;
+};
+
 
 if (process.env.NODE_ENV === 'development') {
   config.entry.unshift('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000');
